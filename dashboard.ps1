7f19a5804317c7208d29b66afdbde614a515004a
Vinodkumar Sunkara (TATA CONSULTANCY SERVICES LTD) <v-vinsun@microsoft.com>
2020-09-03 12:49:20 -0700

    Second

---
+++ dashboard.ps1
@@ -0,0 +1,5 @@
+Import-Module "$PSScriptRoot/UniversalDashboard.psd1"
+Import-Module "$PSScriptRoot/UniversalDashboard.CodeEditor/1.0.0/UniversalDashboard.CodeEditor.psd1"
+Import-Module "$PSScriptRoot/ud-chatroom.psd1"
+
+Start-UDChatroom -Wait
---
+++ deploy.ps1
@@ -0,0 +1,10 @@
+param($Staging)
+
+Copy-Item ./ud-chatroom.psd1 $Staging
+Copy-Item ./ud-chatroom.psm1 $Staging
+Copy-Item ./dashboard.ps1 $Staging
+Save-Module -Name UniversalDashboard -Path $Staging -AcceptLicense -RequiredVersion 2.6 
+
+Copy-Item -Path "$Staging/UniversalDashboard/2.6.0/*" -Destination $Staging -Container -Recurse
+Remove-Item -Path "$Staging/UniversalDashboard" -Force -Recurse 
+Save-Module -Name UniversalDashboard.CodeEditor -Path $Staging -RequiredVersion 1.0
